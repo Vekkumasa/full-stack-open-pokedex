403bf2ac6e512e3b40be2ff3a41bc3cf6e9b62dd
Vekkumasa <ylirasku@gmail.com>
2021-02-25 15:42:26 +0200

    ffssf

--- cypress/integration/pokemonit/pokedex.js
+++ cypress/integration/pokemonit/pokedex.js
@@ -1,13 +1,13 @@
 describe('Pokedex', () => {
-    it('front page can be opened', () => {
-      cy.visit('http://localhost:5000')
-      cy.contains('ivysaur')
-      cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
-    })
-  
-    it('Pokemon can be found', () => {
-      cy.visit('http://localhost:5000')
-      cy.contains('squirtle').click()
-      cy.contains('torrent')
-    })
+  it('front page can be opened', () => {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('Pokemon can be found', () => {
+    cy.visit('http://localhost:5000')
+    cy.contains('squirtle').click()
+    cy.contains('torrent')
+  })
+})
